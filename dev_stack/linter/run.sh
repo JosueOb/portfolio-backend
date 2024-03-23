94b9570ae2efb81c102d75a40a408fe3db68cd98
JosueOb <josuericardocando@hotmail.com>
2024-03-22 21:40:27 -0500

    style: Update linter script

--- dev_stack/linter/run.sh
+++ dev_stack/linter/run.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Running linter"
+echo "Analyzing source code"
 
 # shellcheck disable=SC2034
 PYTHON_FILES=$(find "${PORTFOLIO_WORKDIR_PATH}" -name "*.py")
 
 echo "Running isort"
-isort "${PYTHON_FILES}"
+# shellcheck disable=SC2086
+isort ${PYTHON_FILES}
 
 echo "Running black"
-black "${PYTHON_FILES}"
+# shellcheck disable=SC2086
+black ${PYTHON_FILES}
 
 echo "Running flake8"
-flake8 --config="${LINTER_WORKDIR_PATH}/.flake8" "${PYTHON_FILES}"
+# shellcheck disable=SC2086
+flake8 --config="${LINTER_WORKDIR_PATH}/.flake8" ${PYTHON_FILES}
 
 echo "Running mypy"
-mypy "${PYTHON_FILES}"
+# shellcheck disable=SC2086
+mypy ${PYTHON_FILES}
